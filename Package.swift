b6ded33c22176ee02f08f0ae563fdb1bcb98bf0d
MARTIN Denis <denis@irlmobile.com>
2025-09-02 10:16:16 +0200

    Added XCFramework + Package

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// Package.swift
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+  name: "WeChatOpenSDK",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(name: "WeChatOpenSDK", targets: ["WeChatOpenSDK"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "WeChatOpenSDK",
+      url: "https://your.internal.cdn/WeChatOpenSDK-2.0.5.zip",
+      checksum: "10aa88b4fa297a2b46b59a878b7303aff0c1d6ac7c241259dec6b3ca89424e40"
+    )
+  ]
+)
